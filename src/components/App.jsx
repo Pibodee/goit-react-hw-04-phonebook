be8ec77fea78b9fedda7f3754f9ac0c5288cefbc
Pibodee <mrbodyanuch@gmail.com>
2023-03-16 11:07:43 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,27 +5,24 @@ import { Filter } from './Filter/Filter';
 import { useState, useEffect } from 'react';
 
 export const App = () => {
-  
-  const[contacts, setContacts] = useState(()=> {
-   const savedContacts = localStorage.getItem('contacts')
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem('contacts');
     if (savedContacts !== null) {
       const parsedContacts = JSON.parse(savedContacts);
-      return parsedContacts
-    } 
+      return parsedContacts;
+    }
     return [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
   });
-  const[filter, setFilter] = useState('');
-
-
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts))
-  },[contacts])
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = (values, { resetForm }) => {
     let newContact = values;
@@ -35,7 +32,7 @@ export const App = () => {
     );
 
     if (verification.length) {
-      alert(`${newContact.name} is already in contacts`)
+      alert(`${newContact.name} is already in contacts`);
       return;
     } else {
       newContact.id = nanoid();
@@ -48,33 +45,27 @@ export const App = () => {
     }
   };
 
- const onFiletr = ({ target: { value } }) => {
-    setFilter(value)
+  const onFiletr = ({ target: { value } }) => {
+    setFilter(value);
   };
 
-  const filterContacts = () => {return contacts.filter(({ name }) => {
+  const filterContacts = () => {
+    return contacts.filter(({ name }) => {
       return name.toLowerCase().includes(filter.toLowerCase());
     });
   };
 
   const onDelete = evtId => {
-    setContacts(
-      prevState => prevState.filter(({ id }) => id !== evtId),
-    );
+    setContacts(prevState => prevState.filter(({ id }) => id !== evtId));
   };
 
-  
-    return (
-      <>
-        <h1>Phonebook</h1>
-        <ContactForm onFormSubmit={addContact} />
-        <h2>Contacts</h2>
-        <Filter onInput={onFiletr} />
-        <ContactList
-          filtered={filterContacts()}
-          onDelete={onDelete}
-        />
-      </>
-    );
-  
-}
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm onFormSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter onInput={onFiletr} />
+      <ContactList filtered={filterContacts()} onDelete={onDelete} />
+    </>
+  );
+};
